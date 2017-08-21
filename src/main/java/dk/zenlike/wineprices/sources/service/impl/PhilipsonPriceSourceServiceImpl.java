f02f45637031748385d66a7ad6a991162405c146
Rune Nielsen <runenielsen@gmail.com>
2017-08-21 19:54:20 +0200

    Added a couple of wines

--- src/main/java/dk/zenlike/wineprices/sources/service/impl/PhilipsonPriceSourceServiceImpl.java
+++ src/main/java/dk/zenlike/wineprices/sources/service/impl/PhilipsonPriceSourceServiceImpl.java
@@ -19,8 +19,13 @@ public class PhilipsonPriceSourceServiceImpl implements PriceSourceService {
         PriceSource soulo2013PriceSource = new PriceSourceImpl("Suolo 2013", "http://www.philipsonwine.com/2013-suolo-tenuta-di-argiano/65701313", sourceConfig.getPriceSelector(), sourceConfig.getAmountSelector(), sourceConfig.getNameSelector());
         PriceSource soulo2012PriceSource = new PriceSourceImpl("Suolo 2012", "http://www.philipsonwine.com/2012-suolo-tenuta-di-argiano/65703312", sourceConfig.getPriceSelector(), sourceConfig.getAmountSelector(), sourceConfig.getNameSelector());
 
+        PriceSource guigalPriceSource = new PriceSourceImpl("Guigal St. Joseph", "http://www.philipsonwine.com/2013-saint-joseph-rouge-guigal/47991313", sourceConfig.getPriceSelector(), sourceConfig.getAmountSelector(), sourceConfig.getNameSelector());
+        PriceSource guigalDitPriceSource = new PriceSourceImpl("Guigal St. Joseph Lieu Dit", "http://www.philipsonwine.com/2014-saint-joseph-rouge-lieu-dit-saint-joseph-e-guigal/48001314", sourceConfig.getPriceSelector(), sourceConfig.getAmountSelector(), sourceConfig.getNameSelector());
+
         priceSources.add(soulo2013PriceSource);
         priceSources.add(soulo2012PriceSource);
+        priceSources.add(guigalPriceSource);
+        priceSources.add(guigalDitPriceSource);
 
         return priceSources;
     }
